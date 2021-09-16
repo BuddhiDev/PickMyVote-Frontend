720fbb9c9cde129c1efabc13b93c335244188a77
BuddhiDev <39466972+BuddhiDev@users.noreply.github.com>
2021-09-16 20:30:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/PickMyVote-Frontend'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ng-pick-my-vote'));
 
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/PickMyVote-Frontend/'}),
-// );
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ng-pick-my-vote/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
