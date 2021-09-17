589a2e468f7e02003c7458aec18f641168e6bd31
BuddhiDev <39466972+BuddhiDev@users.noreply.github.com>
2021-09-17 09:20:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/ng-pick-my-vote'));
